a85c2ccf4d25499ebd55683fd01f2e644a847cf0
Pedro Fonseca <pedrotemoteofonseca@gmail.com>
2023-11-25 17:05:23 -0300

    Alteração das classes

--- LinhaProducao/Form1.cs
+++ LinhaProducao/Form1.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using LinhaDeProducao;
+using Listas;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,5 +19,34 @@ namespace LinhaProducao
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                Etapas etapa = new Etapas();
+
+
+                etapa.nome = "PEPE";
+                etapa.ordem = 3;
+                etapa.id_processo = 9;
+
+
+
+                etapa.Insert();
+                
+
+                MessageBox.Show("Cliente adicionado com sucesso");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+
+
     }
 }
+
